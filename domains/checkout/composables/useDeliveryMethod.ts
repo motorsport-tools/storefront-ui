7b87d763493ec23ab7b7bdcf94613c774bcc5025
Tom <51405973+TomJ1588@users.noreply.github.com>
2025-04-10 10:34:21 +0100

    Deploy

--- domains/checkout/composables/useDeliveryMethod.ts
+++ domains/checkout/composables/useDeliveryMethod.ts
@@ -68,39 +68,6 @@ export const useDeliveryMethod = () => {
     return t("shippingMethod.deliveryTime.standard");
   };
 
-  // Current date and time logic
-  const currentDate = new Date();
-  const currentHour = currentDate.getHours();
-  const currentDay = currentDate.getDay();
-
-  const getEstimatedDelivery = (method: ShippingMethod) => {
-    const shippingCountry = cart.value?.order?.partnerShipping?.country?.name || "";
-    if (method.name.toLowerCase().includes("click & collect")) {
-      // Click & Collect logic
-      if (currentDay >= 1 && currentDay <= 5) {
-        if (currentHour >= 8 && currentHour < 15) {
-          return t("shippingMethod.deliveryTime.hour");
-        } else if (currentHour >= 15) {
-          return t("shippingMethod.deliveryTime.tomorrow");
-        }
-      }
-      if (currentDay === 5 && currentHour >= 15) {
-        return t("shippingMethod.deliveryTime.monday");
-      }
-      return t("shippingMethod.deliveryTime.hour");
-    } else if (method.name.toLowerCase().includes("pallet")) {
-      return shippingCountry === "United Kingdom"
-      ? t("shippingMethod.deliveryTime.palletUk")
-      : t("shippingMethod.deliveryTime.palletWorld");
-    } else if (method.name.toLowerCase().includes("delivery")) {
-      return shippingCountry === "United Kingdom"
-      ? t("shippingMethod.deliveryTime.standard")
-      : t("shippingMethod.deliveryTime.international");
-    }
-    // Placeholder: Other methods need separate logic
-    return t("shippingMethod.deliveryTime.standard"); 
-  };
-
   const loadDeliveryMethods = async () => {
     loading.value = true;
     try {

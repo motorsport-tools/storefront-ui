6efaba33cdc73f63dadb0fe6bcf6d0267f67b5de
Tom <51405973+TomJ1588@users.noreply.github.com>
2025-06-12 11:25:26 +0100

    [FIX] config and Dockerfile Build

--- domains/recent-view-products/nuxt.config.ts
+++ domains/recent-view-products/nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxt/test-utils/module'
+      ...(process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'
+        ? ['@nuxt/test-utils/module']
+        : [])
     ]
-})
+  })

43d273fcf9f59da4a5d8ec2fe5b36b3433c20026
Tom <51405973+TomJ1588@users.noreply.github.com>
2025-09-23 13:48:18 +0100

    [FIX] Sitemap cache config ttl 24hrs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -206,11 +206,12 @@ export default defineNuxtConfig({
   },
   sitemap: {
     sources: ['/api/sitemap/urls/products', '/api/sitemap/urls/categories'],
+    cacheMaxAgeSeconds: 3600000,
     runtimeCacheStorage: {
       driver: process.env.NUXT_STORAGE_DRIVER || '',
       url: process.env.NUXT_STORAGE_URL,
       password: process.env.NUXT_STORAGE_PASSWORD,
-      ttl: process.env?.NUXT_SWR_CACHE_TIME || 3600,
+      ttl: 3600000,
     }
   },
   robots: {

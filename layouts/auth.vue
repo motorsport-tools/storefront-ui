06e543089ce71c477d4968f070f81a07169beab1
Tom <51405973+TomJ1588@users.noreply.github.com>
2025-09-25 16:16:30 +0100

    [FIX] Update auth layout - new look

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,25 +1,58 @@
 <script setup lang="ts">
-defineProps<{
-  heading: string;
-}>();
+defineProps({
+  error: Object,
+  heading: String
+})
+
+const {
+	siteData,
+	siteError,
+	refresh,
+} = await useSiteGlobals()
+
+const { isVisualEditingEnabled, apply } = useVisualEditing()
+
+const navigation = ref<HTMLElement>()
+const footer = useTemplateRef('footerRef')
+
+function onNavigationReady(el: HTMLElement) {
+  if(!isVisualEditingEnabled) return
+  navigation.value = el
+  apply({
+    elements: [navigation.value as HTMLElement, footer.value?.footerRef as HTMLElement],
+    onSaved: () => {
+      refresh()
+    },
+  })
+}
 </script>
 
 <template>
-  <TheHeader />
-  <main
-    :class="[
-      'mx-auto px-4 pt-4 pb-20 md:px-0 md:mt-4',
-      { 'md:mb-8': heading },
-      heading ? 'md:max-w-[630px]' : 'md:max-w-[677px]',
-    ]"
-  >
-    <h1
-      v-if="heading"
-      class="font-bold mb-10 typography-headline-3 md:typography-headline-2"
+  <SiteHeader @navigationReady="onNavigationReady" />
+  <div class="relative z-1">
+    <main
+      :class="[
+        'w-full narrow-container bg-white mb-20 px-4 pt-4 pb-20 md:px-0 md:mt-4',
+        { 'md:mb-8': heading },
+        heading ? 'md:max-w-[630px]' : 'md:max-w-[677px]',
+      ]"
     >
-      {{ heading }}
-    </h1>
-    <slot />
-  </main>
-  <TheFooter />
+      <h1
+        v-if="heading"
+        class="font-bold mb-10 typography-headline-3 md:typography-headline-2"
+      >
+        {{ heading }}
+      </h1>
+      <slot />
+    </main>
+  </div>
+  <LazyNewsletter />
+
+    <LazySiteFooter 
+      hydrate-on-visible 
+      ref="footerRef"
+      :navigation="siteData?.footerNavigation || []"
+      :globals="siteData?.globals || {}"
+    />
+    <LazyWishlistSidebar />
 </template>

5c64b5855610384e0d5171d21fbc930889855d23
Tom <51405973+TomJ1588@users.noreply.github.com>
2025-11-04 09:11:18 +0000

    [ADD] Debug

--- layers/cart-redis/server/api/odoo/cart-load.get.ts
+++ layers/cart-redis/server/api/odoo/cart-load.get.ts
@@ -8,6 +8,10 @@ export default defineEventHandler(async (event: any) => {
   const keyName = `cache:cart:session:${session?.id}`
   const data = await useStorage('cart').getItem(keyName);
 
+  console.log('Cart Load called')
+  console.log('Cart: ', keyName)
+  console.log(data)
+
   for (const orderLine of data?.cart?.order?.orderLines || []) {
     const stock = await useStorage('stock').getItem<string>(
       `stock:product-${orderLine?.product?.id}`,

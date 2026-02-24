9ea8563cd3a9c045d13cbbdca26afb898879b35e
Tom <51405973+TomJ1588@users.noreply.github.com>
2026-02-24 10:25:19 +0000

    [FIX] Scripts cleanup on page nav

--- layers/directus/components/Blocks/BlockScript.vue
+++ layers/directus/components/Blocks/BlockScript.vue
@@ -5,19 +5,42 @@ interface Props {
 
 const props = defineProps<Props>()
 const container = ref<HTMLElement | null>(null)
+let script: HTMLScriptElement
+
+const cleanupRecaptcha = () => {
+    document.querySelectorAll('.grecaptcha-badge').forEach(el => el.parentElement?.remove())
+
+    document.querySelectorAll('iframe[src*="recaptcha"]').forEach(el => el.remove())
+
+    document.querySelectorAll('script[src*="recaptcha"]').forEach(el => el.remove())
+}
+
+const cleanupMautic = () => {
+    document.querySelectorAll('iframe[id*=mauticiframe]').forEach(el => el.remove())
+}
+
+
 
 onMounted(() => {
-    const script = document.createElement('script')
+    script = document.createElement('script')
     script.src = props.blockData?.content
     script.async = true
     script.type = 'text/javascript'
     container.value?.appendChild(script)
+})
+onUnmounted(() => {
+    container.value?.removeChild(script)
+})
+onBeforeRouteLeave(() => {
+    cleanupRecaptcha()
+    cleanupMautic()
 })
 </script>
 <template>
     <div 
         ref="container"
         class="block-script"
+        :key="blockData.id"
     >
     </div>
 </template>

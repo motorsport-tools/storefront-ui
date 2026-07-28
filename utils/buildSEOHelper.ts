98b371264ff7229e3c12c647d477d7da346e578f
Tom <51405973+TomJ1588@users.noreply.github.com>
2026-07-28 12:39:12 +0100

    [FIX] Json LD stringify

--- utils/buildSEOHelper.ts
+++ utils/buildSEOHelper.ts
@@ -111,8 +111,7 @@ const generateSeo = <T extends SeoEntity>(entity: T, entityType: string) => {
         script: [
             entity?.jsonLd && {
                 type: "application/ld+json",
-                innerHTML: JSON.stringify(entity.jsonLd),
-                safe: false
+                innerHTML: JSON.stringify(typeof entity.jsonLd === 'string' ? JSON.parse(entity.jsonLd) : entity.jsonLd),
             },
         ].filter(Boolean),
         link: [
